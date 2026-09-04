51e3a6ef3d7f0171d162a90e8e11e46c94950058
Mohaned Kohil <moha.kohil@gmail.com>
2026-09-05 00:38:49 +0300

    Add Google Tag Manager integration to layout
    - Included Google Tag Manager script in the head of the layout for enhanced tracking and analytics.
    - Added noscript fallback for users with JavaScript disabled to ensure tracking continuity.
    
    This update improves the app's ability to gather user interaction data effectively.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Poppins } from "next/font/google"
 import { Geist_Mono } from "next/font/google"
 import { Cormorant_Garamond } from "next/font/google"
+import Script from "next/script"
 import { ThemeProvider } from "@/components/theme-provider"
 import { CartProvider } from "@/lib/cart-context"
 import { Toaster } from "sonner"
@@ -38,7 +39,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5SZM7NXP');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${poppins.variable} ${geistMono.variable} ${cormorant.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5SZM7NXP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider>
           <CartProvider>
             {children}
